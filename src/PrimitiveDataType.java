5ca7221266cd21388998ad468f1a67fffa771587
vishwajeet-rajputt <vishwajeetchhonker@gmail.com>
2025-03-19 11:44:15 +0530

    Day 2

---
+++ src/PrimitiveDataType.java
@@ -0,0 +1,14 @@
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		String name="Vishwajeet Chhonker";
+		String rollNo="2100320100194";
+		String admNo="2021B0101195";
+		Long number=9119734412L;
+		System.out.println(name);
+		System.out.println(rollNo);
+		System.out.println(admNo);
+		System.out.println(number);
+	}
+
+}
